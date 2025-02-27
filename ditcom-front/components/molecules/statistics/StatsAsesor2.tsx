697ff70cb4462f09bfc91e79d759566b3a704677
Camgi264 <mariac.giraldom@autonoma.edu.co>
2025-02-27 18:57:04 -0500

    Correcciones

--- ditcom-front/components/molecules/statistics/StatsAsesor2.tsx
+++ ditcom-front/components/molecules/statistics/StatsAsesor2.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import '@fortawesome/fontawesome-free/css/all.min.css'; // Importa FontAwesome
 
 export default function StatsAsesor2() {
     return (
--- ditcom-front/components/molecules/statistics/StatsPyme2.tsx
+++ ditcom-front/components/molecules/statistics/StatsPyme2.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '@fortawesome/fontawesome-free/css/all.min.css'; 
+
 
 export default function StatsAsesor2() {
     return (
